9004cb9f3eb8bd2bc1d9f001530a7cf297451894
baxodirovF <farxod.baxodirov@webase.uz>
2025-06-15 01:15:05 +0500

    Add Review entity to Domain layer

---
+++ HotelBookingSystem/src/HotelBookingSystem.Domain/Entities/Review.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelBookingSystem.Domain.Entities;
+
+public class Review
+{
+    public long ReviewId { get; set; }
+    public long CustomerId { get; set; }
+    public long HotelId { get; set; }
+    public int Rating { get; set; }
+    public string Comment { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public bool IsVisible { get; set; }
+}
