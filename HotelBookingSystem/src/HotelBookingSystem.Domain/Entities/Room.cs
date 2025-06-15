c101c570e1ed16b263f7c6206036fc33d698379c
Nasiba <nasibaabdulboriyeva71@gmail.com>
2025-06-15 08:58:45 +0500

    Add Room Entity

---
+++ HotelBookingSystem/src/HotelBookingSystem.Domain/Entities/Room.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelBookingSystem.Domain.Entities;
+
+public class Room
+{
+    public long RoomId  { get; set; }
+    public int RoomNumber  { get; set; }
+    public string RoomType  { get; set; }
+    public decimal Price  { get; set; }
+    public bool Availibility { get; set; } 
+
+}
