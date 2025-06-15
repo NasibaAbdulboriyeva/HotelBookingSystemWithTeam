a7389d19aa987eae13ef4e369826e94a562709f6
Nasiba <nasibaabdulboriyeva71@gmail.com>
2025-06-15 15:21:10 +0500

    Add Service Entity

---
+++ HotelBookingSystem/src/HotelBookingSystem.Domain/Entities/Service.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBookingSystem.Domain.Entities;
+
+public class Service
+{
+    public long ServiceId { get; set; }
+    public string ServiceName  { get; set; }
+    public string Description  { get; set; }
+    public decimal Price  { get; set; }
+    public bool IsAvailable { get; set; }
+}
