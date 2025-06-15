bf88e23feef4364bd20c22ac2dd71fe012da65a4
Nasiba <nasibaabdulboriyeva71@gmail.com>
2025-06-15 15:15:43 +0500

    Add Complaint entity

---
+++ HotelBookingSystem/src/HotelBookingSystem.Domain/Entities/Complaint.cs
@@ -0,0 +1,16 @@
+﻿using HotelBookingSystem.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBookingSystem.Domain.Entities;
+
+public class Complaint
+{
+    public long ComplaintId { get; set; }
+    public string Message { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public ComplaintStatus Status { get; set; }
+}
