815ba5733f9d57c3f3ecd444f5e7f120fa3d3ece
Ahliddin <fahriddinovahliddin687@gmail.com>
2025-06-16 23:28:25 +0500

    Added Room configuration

--- HotelBookingSystem/src/HotelBookingSystem.Domain/Entities/Room.cs
+++ HotelBookingSystem/src/HotelBookingSystem.Domain/Entities/Room.cs
@@ -7,6 +7,9 @@ public class Room
     public string RoomType { get; set; }
     public decimal Price { get; set; }
     public bool Availibility { get; set; }
+
     public long HotelId { get; set; }
     public Hotel Hotel { get; set; }
+
+    public List<BookingRoom> BookingRooms { get; set; } = new List<BookingRoom>();
 }
