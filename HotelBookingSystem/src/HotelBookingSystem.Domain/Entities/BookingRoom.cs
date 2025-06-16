4ce221948e436da8aca2ce81625885606beeb254
Ahliddin <fahriddinovahliddin687@gmail.com>
2025-06-16 22:08:33 +0500

    Established-booking-room-many-to-many

---
+++ HotelBookingSystem/src/HotelBookingSystem.Domain/Entities/BookingRoom.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelBookingSystem.Domain.Entities;
+public class BookingRoom
+{
+    public long BookingId { get; set; }
+    public Booking Booking { get; set; }
+
+    public long RoomId { get; set; }
+    public Room Room { get; set; }
+}
