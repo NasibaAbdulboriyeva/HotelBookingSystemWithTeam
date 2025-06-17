9e6b15bc51b606e298a69a0e133f6617599cc1d4
Nasiba <nasibaabdulboriyeva71@gmail.com>
2025-06-17 22:25:50 +0500

    Added Review Configuration

--- HotelBookingSystem/src/HotelBookingSystem.Domain/Entities/Review.cs
+++ HotelBookingSystem/src/HotelBookingSystem.Domain/Entities/Review.cs
@@ -3,7 +3,6 @@
 public class Review
 {
     public long ReviewId { get; set; }
-    public long CustomerId { get; set; }
     public int Rating { get; set; }
     public string Comment { get; set; }
     public DateTime CreatedAt { get; set; }
