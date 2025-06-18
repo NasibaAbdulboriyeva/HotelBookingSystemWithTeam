bfaacb8e438cb04f3c82e3b99ea2a9981fdb4def
baxodirovF <farxod.baxodirov@webase.uz>
2025-06-18 15:54:54 +0500

    add Complaint entity configuration with relationships to Customer and Hotel

---
+++ HotelBookingSystem/src/HotelBookingSystem.Infrastructure/Persistence/Configurations/ComplaintConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using HotelBookingSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HotelBookingSystem.Infrastructure.Persistence.Configurations
+{
+    public class ComplaintConfiguration : IEntityTypeConfiguration<Complaint>
+    {
+        public void Configure(EntityTypeBuilder<Complaint> builder)
+        {
+            builder.HasKey(c => c.ComplaintId);
+
+            builder.Property(c => c.Message)
+                .IsRequired()
+                .HasMaxLength(1000);
+
+            builder.Property(c => c.Status)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(c => c.CreatedAt)
+                .HasDefaultValueSql("GETDATE()");
+
+            builder.HasOne(c => c.User)
+                .WithMany(u => u.Complaints)
+                .HasForeignKey(c => c.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(c => c.Hotel)
+                .WithMany(h => h.Complaints)
+                .HasForeignKey(c => c.HotelId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
