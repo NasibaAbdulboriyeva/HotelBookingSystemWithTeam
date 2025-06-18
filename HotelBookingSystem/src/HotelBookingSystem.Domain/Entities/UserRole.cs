f187bf7831989c24da2b113bbe4fcd42717ac513
Nasiba <nasibaabdulboriyeva71@gmail.com>
2025-06-18 17:20:01 +0500

    added migration

--- HotelBookingSystem/src/HotelBookingSystem.Domain/Entities/UserRole.cs
+++ HotelBookingSystem/src/HotelBookingSystem.Domain/Entities/UserRole.cs
@@ -8,9 +8,9 @@ namespace HotelBookingSystem.Domain.Entities;
 
 public class UserRole
 {
-    public int UserId { get; set; }
+    public long UserId { get; set; }
     public User User { get; set; }
 
-    public int RoleId { get; set; }
+    public long RoleId { get; set; }
     public Role Role { get; set; }
 }
