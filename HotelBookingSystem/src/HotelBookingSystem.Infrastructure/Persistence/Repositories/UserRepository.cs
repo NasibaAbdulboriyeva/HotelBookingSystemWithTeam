b1b6c0cfeedb1de2e33ba4918bfe11b7a0d92068
Po'lat <Po'lat@DEVELOPER>
2025-06-19 16:47:37 +0500

    Implement IUserRepository in Infrastructure

---
+++ HotelBookingSystem/src/HotelBookingSystem.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,57 @@
+﻿using HotelBookingSystem.Application.RepositoryInterfaces;
+using HotelBookingSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBookingSystem.Infrastructure.Persistence.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly AppDbContext _context;
+
+    public UserRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User> SelectByIdAsync(long id)
+    {
+        return await _context.Users
+            .AsNoTracking()
+            .FirstOrDefaultAsync(u => u.UserId == id);
+    }
+
+    public async Task<User> SelectByEmailAsync(string email)
+    {
+        return await _context.Users
+            .AsNoTracking()
+            .FirstOrDefaultAsync(u => u.Email == email);
+    }
+
+    public async Task<User> SelectUserByUserNameAsync(string userName)
+    {
+        return await _context.Users
+            .AsNoTracking()
+            .FirstOrDefaultAsync(u => u.FirstName == userName);
+    }
+
+    public async Task<ICollection<User>> SelectAllUsersAsync(int skip, int take)
+    {
+        return await _context.Users
+            .AsNoTracking()
+            .Skip(skip)
+            .Take(take)
+            .ToListAsync();
+    }
+
+    public async Task InsertAsync(User user)
+    {
+        await _context.Users.AddAsync(user);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(User user)
+    {
+        _context.Users.Update(user);
+        await _context.SaveChangesAsync();
+    }
+}
