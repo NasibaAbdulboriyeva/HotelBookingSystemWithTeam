0aba91f525bb545bfa594c8078dc9ab60cdbe2cd
Ahliddin <fahriddinovahliddin687@gmail.com>
2025-06-19 16:51:02 +0500

    Implemented UserRole repository

---
+++ HotelBookingSystem/src/HotelBookingSystem.Infrastructure/Persistence/Repositories/UserRoleRepository.cs
@@ -0,0 +1,38 @@
+﻿using HotelBookingSystem.Application.RepositoryInterfaces;
+using HotelBookingSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBookingSystem.Infrastructure.Persistence.Repositories;
+public class UserRoleRepository : IUserRoleRepository
+{
+    private readonly AppDbContext _context;
+    public UserRoleRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<ICollection<Role>> SelectRolesByUserIdAsync(long userId)
+    {
+        return await _context.UserRoles
+            .Where(ur => ur.UserId == userId)
+            .Select(ur => ur.Role)
+            .ToListAsync();
+    }
+    public async Task<long> InsertUserRoleAsync(UserRole userRole)
+    {
+        _context.UserRoles.Add(userRole);
+        await _context.SaveChangesAsync();
+        return userRole.RoleId;
+    }
+    public async Task RemoveUserRoleAsync(long userId, long roleId)
+    {
+        var userRole = await _context.UserRoles
+            .FirstOrDefaultAsync(ur => ur.UserId == userId && ur.RoleId == roleId);
+
+        if (userRole != null)
+        {
+            _context.UserRoles.Remove(userRole);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
+
